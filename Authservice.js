38f4dff3077fa26ae2c3e4ed2323b662edfa3268
sesco001 <marcasmatheka@gmail.com>
2025-06-07 18:24:18 +0300

    Create Authservice.js

---
+++ Authservice.js
@@ -0,0 +1,12 @@
+package com.fezchat.auth.repository;
+
+import com.fezchat.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByPhoneNumber(String phoneNumber);
+}
