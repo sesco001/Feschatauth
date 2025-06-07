28f6b43f5c7f15365301bf79915cf5d919eda735
sesco001 <marcasmatheka@gmail.com>
2025-06-07 18:21:15 +0300

    Create user.js

---
+++ user.js
@@ -0,0 +1,11 @@
+package com.fezchat.auth.dto;
+
+public class RegisterRequest {
+    private String phoneNumber;
+    private String password;
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+                            }
